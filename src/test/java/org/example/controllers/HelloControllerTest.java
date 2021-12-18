ed7006ffcc093e358b0cbb7abfe0570a6c7aac95
Tom Lynch <lync0143@gmail.com>
2021-12-18 09:12:18 -0600

    Add a test.

---
+++ src/test/java/org/example/controllers/HelloControllerTest.java
@@ -0,0 +1,25 @@
+package org.example.controllers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    @DisplayName("Verify greeting message")
+    void testHelloMessage() {
+        assertThat(testRestTemplate.getForObject("http://localhost:" + port, String.class)).contains("Hello");
+    }
+}
